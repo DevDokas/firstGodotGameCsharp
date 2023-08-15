1723e51dce0aacc4d9ab7d759249f88def253412
Dokas <iguinhogms@gmail.com>
2023-08-14 21:27:03 -0300

    first commit

---
+++ Player/Player.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+	[Export]
+	private const float MoveSpeed = 200.0f;
+	
+
+	
+
+	public override void _PhysicsProcess(float delta ) 
+	{
+		var motion = new Vector2();
+
+		float moveInput = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+	
+		motion.x = moveInput * MoveSpeed;
+		
+		KinematicCollision2D collision = MoveAndCollide(motion * delta);
+	
+		motion.y = 0;
+		
+		motion.y += 9.8f * delta;
+	}
+}
