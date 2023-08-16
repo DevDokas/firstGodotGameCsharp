2bd9d30b4f6bcfe58a3f62914255eaef86295477
Dokas <iguinhogms@gmail.com>
2023-08-15 21:24:50 -0300

    hotfix/hotfix-para-clone-de-arquivos

--- Player/Player.cs
+++ Player/Player.cs
@@ -8,19 +8,20 @@ public class Player : KinematicBody2D
 	
 
 	
-
+	
 	public override void _PhysicsProcess(float delta ) 
 	{
 		var motion = new Vector2();
 
 		float moveInput = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-	
+
 		motion.x = moveInput * MoveSpeed;
-		
+		motion.y = Input.GetActionStrength("jump");
+
 		KinematicCollision2D collision = MoveAndCollide(motion * delta);
 	
 		motion.y = 0;
-		
+	
 		motion.y += 9.8f * delta;
 	}
 }
