0b9eb72750743a6e0c77391869c323257bdd5be3
Igor Dokai Gomes <igordokai@outlook.com>
2023-08-17 18:18:16 -0300

    feature/gravidade-implementada

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,25 +3,30 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	[Export]
-	private const float MoveSpeed = 200.0f;
-	
+	private const float moveSpeed = 100;
+	private const float gravity = 100;
+	private const float jumpForce = -100;
+    private Vector2 motion = Vector2.Zero;
 
-	
-	
 	public override void _PhysicsProcess(float delta ) 
 	{
-		var motion = new Vector2();
 
 		float moveInput = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
 
-		motion.x = moveInput * MoveSpeed;
-		motion.y = Input.GetActionStrength("jump");
+		motion.x = moveInput * moveSpeed;
+		//motion.y += gravity * delta;
 
+		if (IsOnFloor()) {
+			motion.y = 0;
+			if (Input.IsActionPressed("jump")) {
+				motion.y = jumpForce;
+			}
+		} else {
+			motion.y += gravity * delta;
+		}
+	
 		KinematicCollision2D collision = MoveAndCollide(motion * delta);
+		motion = MoveAndSlide(motion);
 	
-		motion.y = 0;
-	
-		motion.y += 9.8f * delta;
 	}
 }
