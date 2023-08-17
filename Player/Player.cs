0dcb90b887cfda831e9aa0ec7a09347e1b521ef0
Dokas <iguinhogms@gmail.com>
2023-08-17 18:54:56 -0300

    feature/pulo-funcional

--- Player/Player.cs
+++ Player/Player.cs
@@ -6,6 +6,8 @@ public class Player : KinematicBody2D
 	private const float moveSpeed = 100;
 	private const float gravity = 100;
 	private const float jumpForce = -100;
+	private const float maxJumpTime = 0.2f;
+	private float currentJumpTime = 0;
     private Vector2 motion = Vector2.Zero;
 
 	public override void _PhysicsProcess(float delta ) 
@@ -14,19 +16,21 @@ public class Player : KinematicBody2D
 		float moveInput = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
 
 		motion.x = moveInput * moveSpeed;
-		//motion.y += gravity * delta;
 
 		if (IsOnFloor()) {
 			motion.y = 0;
 			if (Input.IsActionPressed("jump")) {
-				motion.y = jumpForce;
+				if (currentJumpTime < maxJumpTime) {
+					motion.y += jumpForce;
+					currentJumpTime += delta;
+				}
 			}
 		} else {
-			motion.y += gravity * delta;
+			motion.y += gravity * delta * 2;
 		}
 	
 		KinematicCollision2D collision = MoveAndCollide(motion * delta);
-		motion = MoveAndSlide(motion);
+		motion = MoveAndSlide(motion, Vector2.Up);
 	
 	}
 }
