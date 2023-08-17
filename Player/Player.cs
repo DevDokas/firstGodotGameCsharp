cae4b146aaab67fe72af0e49660ad48803b6b022
Igor Dokai Gomes <igordokai@outlook.com>
2023-08-17 20:57:02 -0300

    feature/altura-de-pulo-regulado

--- Player/Player.cs
+++ Player/Player.cs
@@ -6,7 +6,7 @@ public class Player : KinematicBody2D
 	private const float moveSpeed = 100;
 	private const float gravity = 100;
 	private const float jumpForce = -100;
-	private const float maxJumpTime = 0.2f;
+	private const float maxJumpSpeed = -200;
 	private float currentJumpTime = 0;
     private Vector2 motion = Vector2.Zero;
 
@@ -20,16 +20,20 @@ public class Player : KinematicBody2D
 		if (IsOnFloor()) {
 			motion.y = 0;
 			if (Input.IsActionPressed("jump")) {
-				if (currentJumpTime < maxJumpTime) {
-					motion.y += jumpForce;
-					currentJumpTime += delta;
-				}
+				motion.y += jumpForce;
+			}
+			if (Input.IsActionPressed("dash")) {
+				motion.x *= 1.75f;
+			}
+			if (Input.IsActionPressed("dash") && Input.IsActionPressed("jump")) {
+				motion.y += jumpForce/7;
+				motion.x *= 1.75f;
 			}
 		} else {
 			motion.y += gravity * delta * 2;
 		}
 	
-		KinematicCollision2D collision = MoveAndCollide(motion * delta);
+		//KinematicCollision2D collision = MoveAndCollide(motion * delta);
 		motion = MoveAndSlide(motion, Vector2.Up);
 	
 	}
